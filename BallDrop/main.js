4f2a724784443a1fd2cf650dbfeb0e8b29de0a89
Sam Liu <ontouchstart@gmail.com>
2017-02-26 14:44:10 -0500

    https://ontouchstart.github.io/D3AFrame/BallDrop/

--- BallDrop/main.js
+++ BallDrop/main.js
@@ -3,12 +3,6 @@ const title = d3.select('head').append('title')
 const scene = d3.select('body').append('a-scene')
   .attr('physics', 'debug: true');
 
-/*
-const camera = scene.append('a-entity')
-  .attr('camera', true)
-  .attr('look-controls', true);
-*/
-
 const floor = scene.append('a-plane')
   .attr('rotation', '-90 0 0')
   .attr('position', '0 0 -15') 
@@ -20,5 +14,5 @@ const floor = scene.append('a-plane')
 const ball = scene.append('a-sphere')
   .attr('position', '0 5 -15')
   .attr('color', 'red')
-  .attr('wireframe', true);
+  .attr('wireframe', true)
   .attr('dynamic-body', 'true');
