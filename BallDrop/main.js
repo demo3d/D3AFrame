c18c897a1b96520cd6a7dbccf8558bb615656b95
Sam Liu <ontouchstart@gmail.com>
2017-02-26 17:13:13 -0500

    multiple balls

--- BallDrop/main.js
+++ BallDrop/main.js
@@ -4,16 +4,26 @@ const scene = d3.select('body').append('a-scene')
   .attr('physics', 'debug: true');
 
 const floor = scene.append('a-plane')
-  .attr('rotation', '-90 5 -5')
+  .attr('rotation', '-90 0 0')
   .attr('position', '0 0 -15') 
   .attr('width', 15)
   .attr('height', 15)
   .attr('color', 'blue')
-  .attr('static-body', '')
-  .attr('wireframe', false);
+  .attr('static-body', '');
 
-const ball = scene.append('a-sphere')
-  .attr('position', '0 5 -15')
-  .attr('color', 'red')
-  .attr('wireframe', true)
-  .attr('dynamic-body', '');
+
+setInterval(function () {
+  const x = d3.randomUniform(1)();
+  const y = 20 + d3.randomUniform(1)();
+  const z = -15 + d3.randomUniform(1)();
+  const position = x + ' ' + y + ' ' + z;
+  const ball = scene.append('a-sphere')
+    .attr('position', position)
+    .attr('color', 'red')
+    .attr('wireframe', true)
+    .attr('dynamic-body', '');
+  setTimeout(function () {
+    ball.remove()
+  }, 60000);
+}, 1000);
+
